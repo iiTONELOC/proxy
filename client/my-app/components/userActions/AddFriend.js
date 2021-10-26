14ca030e602e9ed05a54dee794a703a6b86c68a3
iiTONELOC <anthonytropeano@me.com>
2021-10-26 14:03:04 -0400

    added the AddUser button to the users in range, currently will appear when hovering on a user

--- client/my-app/components/userActions/AddFriend.js
+++ client/my-app/components/userActions/AddFriend.js
@@ -1,9 +1,17 @@
 import { RiUserAddFill } from 'react-icons/ri';
 import Button from '../Button';
 export default function AddFriend(props) {
-
+    function handleAddFriend(e) {
+        e.preventDefault();;
+        alert('Add friend');
+    }
     return (
-        <Button>
+        <Button
+            color={{ color: 'gray-700', hover: 'green-700' }}
+            radius={'rounded-md'}
+            class='text-white text-center p-2'
+            action={{ onClick: handleAddFriend }}
+        >
             <RiUserAddFill />
         </Button>
     )
--- client/my-app/components/userItem/UserItem.js
+++ client/my-app/components/userItem/UserItem.js
@@ -1,12 +1,34 @@
+import { CgOptions } from 'react-icons/cg'
+import { useState, useEffect } from 'react'
+import { hoverHandler } from "../navigation/NavLink";
+import AddFriend from '../userActions/AddFriend';
 export default function UserItem({ user }) {
+    const [isMounted, setMounted] = useState(false);
+    const [hover, setHover] = useState(false);
+    function onHover() {
+        return hoverHandler({ hover, setHover });
+    }
+    useEffect(() => {
+        setMounted(true);
+        return () => setMounted(false)
+    }, [])
+    if (!isMounted) return null
     return (
-        <article key={user.username} className="px-2" style={{ listStyle: 'none' }}>
-            {user.username}
-            <ul >
-                <li className="ml-2" style={{ listStyle: 'none' }}>
-                    {user.location ? `${user.location.city}, ${user.location.state}` : null}
-                </li>
-            </ul>
+        <article
+            key={user.username}
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            className=''
+        >
+            <div className='p-1 flex justify-between items-center'>
+                {user.username}
+                <p className='flex-shrink'>{user.location ? `${user.location.city}, ${user.location.state}` : null}</p>
+                {hover ? <span>
+                    <AddFriend />
+                </span> : null}
+
+            </div>
+
         </article>
     )
 }
