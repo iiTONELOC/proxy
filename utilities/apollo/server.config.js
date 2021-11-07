6cc097ea68b035ba3dc161363d402693c7c97b9f
iiTONELOC <anthonytropeano@me.com>
2021-11-07 14:06:25 -0500

    if this part doesnt work ill remove the ssr for now

--- utilities/apollo/server.config.js
+++ utilities/apollo/server.config.js
@@ -6,6 +6,6 @@ import {
 import cache from "./cache";
 
 
-const serverClient = new ApolloClient({ uri: 'https://proximo-chat.herokuapp.com:3000/graphql', cache: cache() });
+const serverClient = new ApolloClient({ uri: 'http://localhost:3000/graphql', cache: cache() });
 
 export default serverClient;
