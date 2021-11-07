3509ec947914e679385bc4f5c107ba1c95d80809
iiTONELOC <anthonytropeano@me.com>
2021-11-07 12:58:45 -0500

    maybe found the error

--- components/Providers/Chat/index.js
+++ components/Providers/Chat/index.js
@@ -25,7 +25,7 @@ export const ChatProvider = ({ ...props }) => {
     const dispatch = useDispatch();
     useEffect(() => {
         setMounted(true);
-        const nS = io(`https://${window.location.hostname}`);
+        const nS = io(`http://${window.location.hostname}`);
         const newSocket = nS;
         setSocket(newSocket);
         return () => { setMounted(false); setLoggedIn(false); setJoined(false) }
